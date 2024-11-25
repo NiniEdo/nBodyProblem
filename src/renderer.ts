30f239350c98425f49bed4aa7981e2a947d6852c
NiniEdo <edoardo.nini@gmail.com>
2024-11-25 11:44:45 +0100

    enhance renderer with circular motion for spheres and improved lighting

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,33 +1,65 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+const fps = 60; // FPS desiderati
+const fpsInterval = 1000 / fps;
+let then = performance.now();
+
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(new THREE.Color(0x021221), 1);
 
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.SphereGeometry(0.2, 300, 300);
-const material = new THREE.MeshPhysicalMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.SphereGeometry(0.2, 100, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, flatShading: true });
+const cube1 = new THREE.Mesh(geometry, material);
+const cube2 = new THREE.Mesh(geometry, material);
+scene.add(cube1, cube2);
 
-const light = new THREE.AmbientLight(0xffffff, 20);
-scene.add(light);
+const directionalLight = new THREE.HemisphereLight(0xffffff, 10);
+scene.add(directionalLight);
+
+const gridHelper = new THREE.GridHelper(10, 10);
+scene.add(gridHelper);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
 camera.position.z = 5;
 
-function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    controls.update();
-
-    renderer.render(scene, camera);
-}
-
 renderer.setAnimationLoop(animate);
+
+
+let angle = 0;
+const radius = 1; // Raggio del cerchio
+const angularSpeed = Math.PI / 2; // Velocità angolare in radianti al secondo
+function animate(now: number) {
+
+    const elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+        const deltaTime = elapsed / 1000; // Converti millisecondi in secondi
+
+        // Aggiorna l'angolo
+        angle += angularSpeed * deltaTime;
+        angle = angle % (2 * Math.PI);
+
+        // Posizioni delle sfere in moto circolare
+        cube1.position.z = radius * Math.cos(angle);
+        cube1.position.x = radius * Math.sin(angle);
+
+        cube2.position.z = radius * Math.cos(angle + Math.PI);
+        cube2.position.x = radius * Math.sin(angle + Math.PI);
+
+        renderer.render(scene, camera);
+    }
+    controls.update();
+}
